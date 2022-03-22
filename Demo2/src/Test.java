c13425455cb8d75e65a930e148a2bece86cb44eb
annighil <annighil@gmail.com>
2022-03-22 09:12:14 -0700

    added x,y,z variables to test class to hyderabad branch

--- Demo2/src/Test.java
+++ Demo2/src/Test.java
@@ -4,6 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hiiii...");
+		int x=10,y=20,z=30;
+		System.out.println(x+"    "+y+"     "+"     "+z);
 
 	}
 
